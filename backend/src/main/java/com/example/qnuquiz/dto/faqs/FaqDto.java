04681f4bd5afc7679d3dc8bfc6ce14de75df24ce
TrungHC <hochitrung08012004@gmail.com>
2025-11-30 10:58:14 +0700

    create API update, search for FAQ and their unit tests

--- backend/src/main/java/com/example/qnuquiz/dto/faqs/FaqDto.java
+++ backend/src/main/java/com/example/qnuquiz/dto/faqs/FaqDto.java
@@ -2,13 +2,16 @@ package com.example.qnuquiz.dto.faqs;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FaqDto {
-
     private long id;
     private String question;
     private String answer;
--- backend/src/main/java/com/example/qnuquiz/repository/FaqRepository.java
+++ backend/src/main/java/com/example/qnuquiz/repository/FaqRepository.java
@@ -1,9 +1,12 @@
 package com.example.qnuquiz.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.qnuquiz.entity.Faqs;
 
-public interface FaqRepository extends JpaRepository<Faqs, Long> {
-
+public interface FaqRepository extends JpaRepository<Faqs, Long> {	
+	
+	List<Faqs> findByQuestionContainingIgnoreCase(String question);
 }
