e83c4e53f80814a2a5bd5674baf3f7795d5f4143
nhattvim <nhattruong13112000@gmail.com>
2026-01-05 20:34:31 +0700

    refactor: Update countByType method to use a custom query with parameter binding

--- backend/src/main/java/com/example/qnuquiz/repository/QuestionRepository.java
+++ backend/src/main/java/com/example/qnuquiz/repository/QuestionRepository.java
@@ -3,6 +3,8 @@ package com.example.qnuquiz.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.qnuquiz.entity.Questions;
 
@@ -10,5 +12,6 @@ public interface QuestionRepository extends JpaRepository<Questions, Long> {
 
     List<Questions> findByExamsId(Long id);
 
-    long countByType(String type);
+    @Query("SELECT COUNT(q) FROM Questions q WHERE CAST(q.type AS string) = :type")
+    long countByType(@Param("type") String type);
 }
